9a608258c77584aac537670b527e62258c2fd535
mayagat2002 <mayagat2002@gmail.com>
2025-11-30 15:46:29 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,24 +1,11 @@
 def create_codon_dict(file_path):
-    codon_dict = {}
-
-    with open(file_path, 'r') as f:
-        for line in f:
-            line = line.strip()
-
-            # דילוג על שורות ריקות או הערות
-            if not line or line.startswith("#"):
-                continue
-
-            # פיצול לשדות
-            parts = line.split()
-
-            # נוודא שיש לפחות קודון ואמין-אסיד
-            if len(parts) < 2:
-                continue
-
-            codon = parts[0].upper()
-            amino_acid = parts[1]
-
-            codon_dict[codon] = amino_acid
-
-    return codon_dict
+    file = open(file_path)
+    rows = file.readlines()
+    file.close()
+    map = {}
+    for row in rows:
+     cells = row.strip().split('\t')
+     codon = cells[0]
+     amino_acid = cells[2]
+     map[codon] = amino_acid
+    return map
